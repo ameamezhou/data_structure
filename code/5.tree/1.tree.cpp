f0788be3aa03a271f1753396a6b6a811fd900b59
ameamezhou <wxz17352825505@163.com>
2023-11-09 21:50:26 +0800

    tree start

--- code/5.tree/1.tree.cpp
+++ code/5.tree/1.tree.cpp
@@ -98,3 +98,64 @@ void levelOrder(BiTree T) {
 			EnQueue(Q, p->rchild);
 	}
 }
+
+// 线索二叉树
+typedef struct ThreadNode {
+	ElemType data;
+	struct ThreadNode* lchild, * rchild;
+	int ltag, rtag;							// tag == 0 表示指向的是孩子，1表示指向的是线索
+}ThreadNode, *ThreadThree;
+
+// 二叉树线索化代码实现
+
+
+// 辅助全局变量  用于查找结点p的前驱
+ThreadNode* p;
+
+ThreadNode* final = NULL;
+
+void visit(ThreadNode* q) {
+	if (q->lchild == NULL) { // 左子树为空，建立前驱线索
+		q->lchild = pre;
+		q->ltag = 1;
+	}
+	if (pre != NULL && pre->rchild == NULL) {
+		pre->rchild = q;
+		pre->rtag = 1;
+	}
+	pre = q;
+}
+
+// 线序线索化的话这里改成先序遍历的逻辑就行，后序也一样
+// 
+// 土方法找到中序前驱
+void MidOrder(BiTree T) {
+	if (T != NULL) {
+		MidOrder(T->lchild);
+		visit(T);
+		MidOrder(T->rchild);
+	}
+}
+ThreadNode* pre = NULL;
+// 中序线索化二叉树T
+void CreateInThread(ThreadNode T) {
+    pre = NULL;
+	if (T != NULL) {
+		MidOrder(T);
+		if (pre->rchild == NULL)
+			pre->rtag = 1;
+	}
+}
+
+// 这里构建线序线索化时，会有无限循环的问题，因为是先根后左，而visit会在根结点的时候
+// 给左结点增加一个指向，下一步往左结点走的时候会走到上一个结点 (自己画图分析下)
+// 就会有问题  所以pre这里还有一步要修改下
+// 
+void PreThread(ThreadNode T) {
+	if (T != NULL) {
+		visit(T);
+		if (T->ltag==0)
+			PreOrder(T->lchild);  // lchild 不能是前驱结点
+		PreOrder(T->rchild);
+	}
+}
