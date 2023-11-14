10bb8e8d541e6db2d6786123d844980880d0952a
ameamezhou <wxz17352825505@163.com>
2023-11-14 13:36:33 +0800

    algorithm tree

--- code/5.tree/3.treeAlgorithm.cpp
+++ code/5.tree/3.treeAlgorithm.cpp
@@ -81,6 +81,41 @@ int width(BiTree T) {
 }
 
 // 在二叉树中查找值为 x 的结点，试编写算法打印值为 x 的结点的所有祖先(值为 x 的结点不多于一个)
+void PrintAncester(BiTree T, int x) {
+    BiTNode* s[1000]; // 局部设置一个栈
+    int top = -1;     // 栈顶指针   
+    BiTNode *p = T, *r = NULL; // r用来记录上一次弹出的结点，不能一直陷入右节点循环
+    while (top > -1|| p != NULL) {
+        if (p != NULL) {
+            s[++top] = p;
+            p = p->lchild;
+        } else {
+            p = s[top];
+            if (p->rchild != NULL && p->rchild != r) {
+                p = p->rchild; // 这里如果不设置弹出条件  就会有右节点弹出，回到上一节点，然后继续发现右节点存在，继续访问，变成死循环
+            } else {
+                // visit
+                if (p->data == x) {
+                    if (top == 0) {
+                        // top = 0 说明是根节点  没有祖先结点
+                        // printf 说明情况
+                        return;
+                    }
+                    for(int i = 0; i < top; ++i) {
+                        // printf("%d", s[i]->data);
+                    }
+                    // printf("\n");
+                    return;
+                }
+                --top;
+                r = p;
+                p = NULL;
+            }
+        }
+    }
+    // 如果在这里还没找到x 那就说明不存在
+    // printf("没有x结点")
+}
 
 
 // 给出二叉树的中序和后序遍历还原二叉树(pipioj1269)
@@ -102,3 +137,71 @@ BiTNode *CreateBiTreeByInAndPost(int in[], int inl, int inr, int post[], int pos
     return T;
 }
 
+// 结点与祖先结点的最大差值
+// 非递归算法
+// 这里直接和找x结点一样   修改一下visit部分的内容就行 
+// 采用后续遍历，这样就能让祖先结点都存在栈中
+int findMaxDiff(BiTree T) {
+    BiTNode* s[1000]; // 局部设置一个栈
+    int top = -1;     // 栈顶指针   
+    BiTNode *p = T, *r = NULL;
+    int max = 0, m = 0; // max记录最大值， m 为循环比较值
+    while (top > -1 || p != NULL) {
+        if (p!=NULL) {
+            s[++top] = p;
+            p = p->lchild;
+        }else {
+            p = s[top];
+            if (p->rchild != NULL && p->rchild != r) {
+                p = p->rchild;
+            } else {
+                // visit
+                for (int i = 0; i < top; i++) {
+                    m = abs(s[i]->data - p->data);
+                    if (m >= max) {
+                        max = m;
+                    }
+                }
+                --top;
+                r = p;
+                p = NULL;
+            }
+        }
+    }
+    return max;
+}
+
+// 递归算法求最大差值
+int result = 0;
+void findMaxDiff2(BiTree T, int max, int min) {
+    if (T==NULL) {
+        return;
+    }
+    if (T->data >= max) {
+        max = T->data;
+    }
+    if (T->data <= min) {
+        min = T->data;
+    }
+    if (max - min > result) {
+        result = max - min;
+    }
+
+    findMaxDiff2(T->lchild, max, min);
+    findMaxDiff2(T->rchild, max, min);
+}
+
+// 求二叉树的直径
+// 什么是二叉树的直径？
+// 直径长度是任意两个节点路径长度中的最大值
+// 就等于说如果我左子树特别大  右子树特别短  我们就可以只看左子树的直径 = 二叉树的直径
+// 用全局变量存储最大直径长
+int Diameter = 0;
+int CalculateDiameter(BiTree T) {
+    if (T == NULL) return 0;
+
+    int ld = CalculateDiameter(T->lchild);
+    int rd = CalculateDiameter(T->rchild);
+    Diameter = Diameter>(ld+rd)?Diameter:(ld+rd);
+    return (ld>rd?ld:rd) + 1;
+}
